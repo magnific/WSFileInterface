e859d2deb118cd5672258d41a0c1d5b722d9bc77
Juan Ramon Mendoza Ramirez <juan_ramon_m@hotmail.com>
2016-07-11 01:56:13 -0500

    .

---
+++ CLibFileManagement/CDirectoryAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CLibFileManagement
+{
+    class CDirectoryAccess : CFileDataAccess
+    {
+
+        private string l_filename = "test.txt";
+        private string l_sourcePath = @"c:\PublicFolder";
+        private string l_targetPath = @"c:\PublicFolder\Detino";
+
+        public override void Move()
+        {
+            string l_sourceFile = Path.Combine(l_sourcePath, l_filename);
+            string l_targetFile = Path.Combine(l_targetPath, l_filename);
+            if (Directory.Exists(l_targetPath))
+            {
+                Directory.CreateDirectory(l_targetPath);
+            }
+
+            File.Move(l_sourceFile, l_targetPath);
+            //throw new NotImplementedException();
+        }
+
+    }
+}
